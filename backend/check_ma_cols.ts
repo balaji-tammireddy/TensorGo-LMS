72957b7407a33290ecb5399a60fdf4485fee3d59
G Sai Jaiwanth <saijaiwanth@gmail.com>
2026-01-27 09:30:39 +0000

    unwanted files removed

--- backend/check_ma_cols.ts
+++
@@ -1,8 +0,0 @@
-
-import { pool } from './src/database/db';
-async function run() {
-    const res = await pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'module_access'");
-    console.table(res.rows);
-    await pool.end();
-}
-run();
